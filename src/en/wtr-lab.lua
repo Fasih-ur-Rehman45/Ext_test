e81491c4d89ec38c18753c1c24a0b196f0db9be2
Fasih-ur-Rehman45 <s2023266121@umt.edu.pk>
2024-09-02 20:36:52 +0500

    Added wtr-lab.lua

---
+++ src/en/wtr-lab.lua
@@ -0,0 +1,127 @@
+-- {"id":10255,"ver":"1.0.0","libVer":"1.0.0","author":""}
+
+
+local baseURL = "https://wtr-lab.com/"
+
+--- @param url string
+--- @param type int 
+local function shrinkURL(url)
+    return url:gsub(baseURL, "")
+end
+
+--- @param url string
+--- @param type int 
+local function expandURL(url)
+    return baseURL .. url
+end
+
+--- @type Filter[] | Array
+local searchFilters = {
+    DropdownFilter(1, "Order by", { "View", "Name", "Addition Date", "Reader", "Chapter" }),
+    DropdownFilter(2, "Sort by", { "Descending", "Ascending" }),
+    DropdownFilter(3, "Status", { "All", "Ongoing", "Completed" })
+}
+
+--- @type ChapterType
+local chapterType = ChapterType.HTML
+--- @type Listing[] | Array
+local listings = {
+    Listing("Popular Novels", true, function(data)
+        --- @type int
+        local page = data[PAGE]
+        local url = baseURL .. "en/novel-list?orderBy=" .. data[1] .. "&order=" .. data[2] .. "&filter=" .. data[3] .. "&page=" .. page
+        local document = GETDocument(url)
+        local novels = {}
+        for i, element in ipairs(document:select(".serie-item")) do
+            local novel = {
+                name = element:selectFirst(".title-wrap > a"):text(),
+                cover = element:selectFirst("img"):attr("src"),
+                path = element:selectFirst("a"):attr("href")
+            }
+            table.insert(novels, novel)
+        end
+        return novels
+    end)
+}
+--- @param chapterURL string 
+--- @return string Strings 
+local function getPassage(chapterURL)
+    local url = expandURL(chapterURL, KEY_CHAPTER_URL)
+    local document = GETDocument(url)
+    local chapterJson = document:selectFirst("#__NEXT_DATA__"):html()
+    local jsonData = JSONDecode(chapterJson)
+    local chapterContent = JSONDecode(jsonData.props.pageProps.serie.chapter_data.data.body)
+    local htmlString = ""
+    for i, text in ipairs(chapterContent) do
+        htmlString = htmlString .. "<p>" .. text .. "</p>"
+    end
+
+    return htmlString
+end
+
+--- @param novelURL string
+--- @return NovelInfo
+local function parseNovel(novelURL)
+    local url = expandURL(novelURL, KEY_NOVEL_URL)
+    local document = GETDocument(url)
+    return NovelInfo {
+    title = document:selectFirst("h1.text-uppercase"):text(),
+    imageURL = document:selectFirst(".img-wrap > img"):attr("src"),
+    description = document:selectFirst(".lead"):text():trim(),
+    genres = document:selectFirst("td:contains('Genre')"):next():select("a"):eachText():join(", "),
+    author = document:selectFirst("td:contains('Author')"):next():text():gsub("[%t\n]", ""),
+    
+    status =({
+    Ongoing = NovelStatus.PUBLISHING,
+    Completed = NovelStatus.COMPLETED,})
+    [document:selectFirst("td:contains('Status')"):next():text():gsub("[%t\n]", "")]
+    }
+end
+    -- Parse chapters
+    local chapterJson = document:selectFirst("#__NEXT_DATA__"):html()
+    local jsonData = JSONDecode(chapterJson)
+    local chapters = jsonData.props.pageProps.serie.chapters
+    for i, jsonChapter in ipairs(chapters) do
+        novel:addChapter(ChapterItem {
+            name = jsonChapter.title,
+            path = "en/serie-" .. jsonData.props.pageProps.serie.serie_data.raw_id .. "/" .. jsonData.props.pageProps.serie.serie_data.slug .. "/chapter-" .. jsonChapter.order,
+            releaseTime = jsonChapter.created_at:sub(1, 10)
+        })
+        return novel
+    end
+
+--- @param data table @of applied filter values [QUERY] is the search query, may be empty.
+--- @return Novel[] | Array
+
+local function search(data)
+    local url = baseURL .. "api/search"
+    local response = POST(url, { text = data[QUERY] })
+    local recentNovel = JSONDecode(response)
+    local novels = {}
+    for i, datum in ipairs(recentNovel.data) do
+        local novel = {
+            name = datum.data.title,
+            cover = datum.data.image,
+            path = "en/serie-" .. datum.raw_id .. "/" .. datum.slug
+        }
+        table.insert(novels, novel)
+    end
+
+    return novels
+end
+
+return {
+    id = 10255,
+    name = "WTR-LAB",
+    baseURL = baseURL,
+    imageURL = "https://wtr-lab.com/flask-white.svg",
+    hasSearch = true,
+    search = search,
+    listings = listings,
+    getPassage = getPassage,
+    chapterType = chapterType.HTML,
+    parseNovel = parseNovel,
+    shrinkURL = shrinkURL,
+    expandURL = expandURL,
+    searchFilters = searchFilters
+}
